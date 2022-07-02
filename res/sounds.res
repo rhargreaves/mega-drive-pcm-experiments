76066a95e743b3d9cc752f2df8ea754efb317b23
Robert Hargreaves <kiyouta@gmail.com>
2022-07-03 00:28:57 +0100

    Play snare at boot

---
+++ res/sounds.res
@@ -0,0 +1,4 @@
+WAV hat1_16k "hat1.wav" 2
+WAV hat2_16k "hat2.wav" 2
+WAV snare1_16k "snare1.wav" 2
+WAV snare2_16k "snare2.wav" 2
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <genesis.h>
+#include "sounds.h"
 
 int main()
 {
+    SND_startPlay_4PCM(hat1_16k, sizeof(hat1_16k), SOUND_PCM_CH_AUTO, FALSE);
+
     while(TRUE) {
-        VDP_waitVSync();
+        SYS_doVBlankProcess();
     }
 }
